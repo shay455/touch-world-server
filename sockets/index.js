ab3cd7d10202d37068fd150f2d05777782dfe355
orbaluu87 <orbalulu8780@gmail.com>
2025-10-10 12:56:26 +0300

    Update index.js

--- sockets/index.js
+++ sockets/index.js
@@ -1,1 +1,32 @@
+// sockets/index.js
+const registerPlayerEvents = require('./handlers/player');
+const registerChatEvents = require('./handlers/chat');
+const registerTradeEvents = require('./handlers/trade');
+
+module.exports = (io, players, createDefaultPlayer, safePlayerView, mergeRuntimeUpdate) => {
+  io.on('connection', (socket) => {
+    console.log(`[+] Player connected: ${socket.id}`);
+
+    // יצירת שחקן חדש
+    players[socket.id] = createDefaultPlayer(socket.id);
+
+    // שלח את כל השחקנים הקיימים למתחבר
+    socket.emit('current_players', players);
+
+    // עדכן את כל השאר על שחקן חדש
+    socket.broadcast.emit('player_joined', safePlayerView(players[socket.id]));
+
+    // רישום אירועים
+    registerPlayerEvents(io, socket, players, safePlayerView, mergeRuntimeUpdate);
+    registerChatEvents(io, socket, players);
+    registerTradeEvents(io, socket, players);
+
+    // ניתוק
+    socket.on('disconnect', () => {
+      console.log(`[-] Player disconnected: ${socket.id}`);
+      delete players[socket.id];
+      io.emit('player_disconnected', socket.id);
+    });
+  });
+};
 
