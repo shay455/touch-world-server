66204796c45497dfc3c611c8c69381331ce8c64e
orbaluu87 <orbalulu8780@gmail.com>
2025-10-10 12:55:36 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,14 @@
+// config/config.js
+const allowedOrigins = [
+  'https://touch-world-server.onrender.com',
+  'https://touch-world.io',
+  'http://localhost:5173',
+  'http://localhost:8081'
+];
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  SOCKET_PATH: '/socket.io',
+  allowedOrigins
+};
 
