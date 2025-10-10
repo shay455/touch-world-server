d03c95d0b1c32a0d2d060926391c8fce9159daad
orbaluu87 <orbalulu8780@gmail.com>
2025-10-10 12:55:49 +0300

    Update cors.js

--- core/cors.js
+++ core/cors.js
@@ -1,1 +1,15 @@
+// core/cors.js
+const cors = require('cors');
+const { allowedOrigins } = require('../config/config');
+
+module.exports = cors({
+  origin: (origin, cb) => {
+    if (!origin) return cb(null, true);
+    return allowedOrigins.includes(origin)
+      ? cb(null, true)
+      : cb(new Error('CORS blocked: ' + origin));
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  credentials: true
+});
 
