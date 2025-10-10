4fb2b185120837ddd217ad59b9c0d869b3046e4c
orbaluu87 <orbalulu8780@gmail.com>
2025-10-10 13:39:01 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,7 @@
-// הגדרות כלליות
-module.exports = {
-  PORT: process.env.PORT || 8080,
-  // שים לב: בלי סלאש בסוף ה-Origin
-  ALLOWED_ORIGINS: [
-    'https://touch-world-server.onrender.com',
-    'https://touch-world.io',
-    'http://localhost:5173',
-    'http://localhost:8081'
-  ]
-};
+// config/config.js
+module.exports.allowedOrigins = [
+  'https://touch-world-server.onrender.com',
+  'https://touch-world.io',
+  'http://localhost:5173',
+  'http://localhost:8081',
+];
