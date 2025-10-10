ce7e76030b57783a2e408fd29872225debc83ee5
orbaluu87 <orbalulu8780@gmail.com>
2025-10-10 13:09:24 +0300

    Update trade.js

--- sockets/handlers/trade.js
+++ sockets/handlers/trade.js
@@ -1,21 +1,25 @@
-// sockets/handlers/trade.js
-module.exports = (io, socket) => {
+module.exports = function registerTradeHandlers(io, socket) {
+  // בקשת טרייד
   socket.on('trade_request', (data = {}) => {
     const { tradeId, initiatorId, receiverId } = data;
     if (receiverId) {
+      console.log(`[TRADE] Request ${tradeId} from ${initiatorId} to ${receiverId}`);
       io.to(receiverId).emit('trade_request_received', data);
     }
   });
 
+  // עדכון סטטוס טרייד
   socket.on('trade_update', (data = {}) => {
     const { tradeId, status, tradeDetails } = data;
+    console.log(`[TRADE] Update trade ${tradeId}, status: ${status}`);
+
     if (tradeDetails) {
-      const otherId =
-        socket.id === tradeDetails.initiator_id
-          ? tradeDetails.receiver_id
-          : tradeDetails.initiator_id;
-      if (otherId) io.to(otherId).emit('trade_status_updated', data);
+      const otherPlayerId =
+        socket.id === (tradeDetails.initiator_id || tradeDetails.initiitorId)
+          ? (tradeDetails.receiver_id || tradeDetails.receiverId)
+          : (tradeDetails.initiator_id || tradeDetails.initiatorId);
+
+      if (otherPlayerId) io.to(otherPlayerId).emit('trade_status_updated', data);
     }
   });
 };
-
