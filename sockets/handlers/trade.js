cb2742d4cb9c64dda5937b2849cc0d1127024548
orbaluu87 <orbalulu8780@gmail.com>
2025-10-10 12:57:13 +0300

    Update trade.js

--- sockets/handlers/trade.js
+++ sockets/handlers/trade.js
@@ -1,1 +1,21 @@
+// sockets/handlers/trade.js
+module.exports = (io, socket) => {
+  socket.on('trade_request', (data = {}) => {
+    const { tradeId, initiatorId, receiverId } = data;
+    if (receiverId) {
+      io.to(receiverId).emit('trade_request_received', data);
+    }
+  });
+
+  socket.on('trade_update', (data = {}) => {
+    const { tradeId, status, tradeDetails } = data;
+    if (tradeDetails) {
+      const otherId =
+        socket.id === tradeDetails.initiator_id
+          ? tradeDetails.receiver_id
+          : tradeDetails.initiator_id;
+      if (otherId) io.to(otherId).emit('trade_status_updated', data);
+    }
+  });
+};
 
