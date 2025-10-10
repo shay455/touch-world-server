a76c0a1cde03d55ae8c0adfa8b754c9c3c4efc17
orbaluu87 <orbalulu8780@gmail.com>
2025-10-10 13:08:09 +0300

    Update players.js

--- core/players.js
+++ core/players.js
@@ -1,13 +1,18 @@
-// core/players.js
+// Store בזיכרון + פונקציות עזר לשחקנים
+
 const players = Object.create(null);
 
-const ALLOWED_FIELDS = new Set([
-  'position_x', 'position_y', 'direction', 'animation_frame',
-  'is_moving', 'move_type', 'username', 'current_area',
-  'is_invisible', 'keep_away_mode'
+// שדות שמותר לעדכן מתוך player_update (תנועה/אנימציה בלבד)
+const RUNTIME_FIELDS = new Set([
+  'position_x',
+  'position_y',
+  'direction',
+  'animation_frame',
+  'is_moving',
+  'move_type'
 ]);
 
-function createDefaultPlayer(id) {
+function makeDefaultPlayer(id) {
   return {
     id,
     username: '',
@@ -18,37 +23,44 @@ function createDefaultPlayer(id) {
     is_moving: false,
     move_type: 'walk',
     current_area: 'city',
-    equipment: {},
+    equipment: {},           // { hat: "hat_01", body: "skin_brown", ... }
+    skin_code: 'blue',
+    admin_level: 'user',
     is_invisible: false,
     keep_away_mode: false,
-    admin_level: 'user',
-    skin_code: 'blue'
+    ready: false             // לא משדרים לשאר עד identify
   };
+}
+
+function mergeRuntime(dst, src = {}) {
+  for (const k of Object.keys(src)) {
+    if (RUNTIME_FIELDS.has(k)) dst[k] = src[k];
+  }
 }
 
 function safePlayerView(p) {
   if (!p) return null;
   return {
     id: p.id,
-    username: p.username,
+    username: p.username || '',
     position_x: p.position_x,
     position_y: p.position_y,
     direction: p.direction,
     animation_frame: p.animation_frame,
     is_moving: p.is_moving,
-    move_type: p.move_type,
-    current_area: p.current_area,
-    equipment: p.equipment,
-    admin_level: p.admin_level,
-    skin_code: p.skin_code
+    move_type: p.move_type || 'walk',
+    current_area: p.current_area || 'city',
+    equipment: p.equipment || {},
+    skin_code: p.skin_code || 'blue',
+    admin_level: p.admin_level || 'user',
+    is_invisible: !!p.is_invisible,
+    keep_away_mode: !!p.keep_away_mode
   };
 }
 
-function mergeRuntimeUpdate(dst, src) {
-  for (const key of Object.keys(src || {})) {
-    if (ALLOWED_FIELDS.has(key)) dst[key] = src[key];
-  }
-}
-
-module.exports = { players, createDefaultPlayer, safePlayerView, mergeRuntimeUpdate };
-
+module.exports = {
+  players,
+  makeDefaultPlayer,
+  mergeRuntime,
+  safePlayerView
+};
